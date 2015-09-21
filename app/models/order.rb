f298c58d5792d8a5931c57a26984056f8c7d49fc
KIGOTHO <mkigotho2@gmail.com>
2015-09-21 15:56:22 +0300

    creating order items auto calculation

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,6 +1,6 @@
 class Order < ActiveRecord::Base
 has_many :order_items, dependent: :destroy    
-    
+
    def total
        
    end
--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -2,7 +2,7 @@ class OrderItem < ActiveRecord::Base
     belongs_to :order
     belongs_to :product 
     
-    
+
     
     def price
         product.price 
